aee687da60ac0d1d35bc337de488b82cae929a9f
Adam <adam.crespo101@outlook.com>
2022-02-25 13:29:57 -0500

    commit tree cb6338bd65fe270263b2dc0e8491702f93a1cfb2

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+let employee ={
+    key: ' ',
+    value: ' '
+}; 
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+        const updateEmployee = {...employee};
+            updateEmployee[key] = value;
+
+            return updateEmployee;
+ }
+
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+             employee[key] = value;
+
+         return employee
+ }
+
+ function deleteFromEmployeeByKey(employee, key){
+    const deleteEmployee = {...employee}
+        delete deleteEmployee.name
+
+     return deleteEmployee
+ }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key){
+      delete employee.name
+    
+          return employee
+  }
